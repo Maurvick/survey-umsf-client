456a8b475ee9e2343719d06a99f2443265c611fd
Maurvick <82706197+Maurvick@users.noreply.github.com>
2024-08-05 16:17:59 +0300

    update app imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 import { SurveyFormComponent } from './components/survey-form/survey-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SurveyFormComponent],
+  imports: [
+    RouterOutlet,
+    SurveyFormComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
