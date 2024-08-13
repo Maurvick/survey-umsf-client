ab3121bdf0a749434f8d65eb1fea82cdee6912fb
Maurvick <82706197+Maurvick@users.noreply.github.com>
2024-08-13 10:22:56 +0300

    update table component

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-table',
@@ -8,12 +8,36 @@ import { Component, Input } from '@angular/core';
   templateUrl: './table.component.html',
   styleUrl: './table.component.css',
 })
-export class TableComponent {
+export class TableComponent implements OnChanges {
   @Input({ required: true }) headers: string[] = [];
-  @Input({ required: true }) rows: any = [];
+  @Input({ required: true }) rows: string[][] = [];
+
+  constructor() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.extractValues(this.rows);
+  }
 
   isTwoDimensionalArray(arr: any): boolean {
     // Check if the first element is an array
     return Array.isArray(arr) && Array.isArray(arr[0]);
+  }
+
+  extractValues(arr: any[]): void {
+    let extractedValues: any[] = [];
+    let sortedRows: any[][] = [];
+    let itemsPerRow = this.headers.length;
+
+    extractedValues = arr
+      .flatMap((obj) => Object.values(obj))
+      .map((item) => String(item));
+
+    for (let i = 0; i < extractedValues.length; i += itemsPerRow) {
+      sortedRows.push(extractedValues.slice(i, i + itemsPerRow));
+    }
+
+    console.log(sortedRows);
+
+    this.rows = sortedRows;
   }
 }
